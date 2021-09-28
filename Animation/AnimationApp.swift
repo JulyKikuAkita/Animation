e1b01cbfb8bac46d388c3d72ab95618299c749f0
JulyKikuAkita <b92701105@gmail.com>
2021-09-28 13:22:26 -0700

    Initial Commit

---
+++ Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Ifang Lee on 9/28/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
